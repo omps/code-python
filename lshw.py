fe1e7c622117ecf21e5016ce540b68bfb2a38120
Om Prakash <ompnix@gmail.com>
2014-11-09 22:06:33 +0530

    Hard disk information collector in python

--- lshw.py
+++ lshw.py
@@ -1,6 +1,32 @@
 #!/usr/bin/python -tt
 # Parse lshw output and collect hardware data.
 
+# Code is taken from https://gist.github.com/amitsaha/4554484
+# I need to modify and pick various other part such as cpu, memory.
+
+
+from lxml import etree
+from subprocess import Popen,PIPE
+inventory = Popen(['lshw', '-xml', '-numeric'], stdout=PIPE).communicate()[0]
+inventory = etree.XML(inventory)
+ 
+find_disks = etree.XPath(".//node[@class='disk']")
+ 
+numdisks = 0
+diskspace = 0
+for disk in find_disks(inventory):
+    # has to be a hard-disk
+    if disk.find('size') is not None:
+        numdisks = numdisks + 1
+        diskspace = diskspace + int(disk.find('size').text)
+        print disk.find('description').text, disk.find('product').text, disk.find('logicalname').text
+        print 'Disk Space: ', disk.find('size').text
+        print 'Sector size: ',disk.find('configuration/setting/[@id="sectorsize"]').get('value')
+        print 
+ 
+print 'Num disks', numdisks
+print 'Total disk space', diskspace/(1024**2)
+
 '''
 Sample output:
 
@@ -14,4 +40,22 @@ inventory = etree.XML(inventory)
 
 # checking only for the disk here, subsequent checks for other hardware parts need to be done along.
 
-find.disks = etree.xpath
+find_disks = etree.Xpath(".//node[@class='disk']")
+
+
+numdisk = 0
+diskspace = 0
+
+for disk in find_disks(inventory):
+    #check if this is hard disk or something else
+    if disk.find('size') is not None:
+        numdisk = numdisk + 1
+        diskspace = diskspace + int(disk.find('size').text)
+        print disk.find('description').text, disk.find('product').text, disk.find('logicalname').text
+        print 'Disk Space: ', disk.find('size').text)
+        print 'Sector Size: ', disk.find('configuration/setting/[@id="sectorsize"]').get('value')
+        print
+
+    print 'Num Disks: ', numdisk
+    print 'Total Disk Space', diskspace/(1024*2)
+
