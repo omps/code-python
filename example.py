913d993ff1ea695b8bc431e387f5c6e11e519610
omps.thinkpad.perl <ompnix@gmail.com>
2015-02-04 22:43:27 +0530

    resbased

--- example.py
+++ example.py
@@ -4,3 +4,12 @@ for w in a:
     print w
     if w > 66:
         print w * w
+#/usr/bin/python2.7 -tt
+# google python class day1.
+
+# define the main function
+
+def main():
+    print "Hello"
+
+# this is the standard bolierplate.
